8bd7a430148cc48d9b6e37f31143c604d27dd301
rodis00 <happyekkov@gmail.com>
2025-04-12 15:55:46 +0200

    add pagination, connect django rest framework

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -41,8 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my apps
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
