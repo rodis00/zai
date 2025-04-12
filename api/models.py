04b8f1c6cef7e89509e1c402f5fff78efc7762c5
rodis00 <happyekkov@gmail.com>
2025-04-12 13:27:24 +0200

    add models and migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    city = models.CharField(max_length=120)
+    country = models.CharField(max_length=120)
+    zipcode = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f"{self.city}, {self.country} ({self.zipcode})"
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='dishes')
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    description = models.TextField()
+    picture = models.ImageField(upload_to='dish_pictures')
+
+    def __str__(self):
+        return f"{self.name} ({self.restaurant.name})"
+
+
+PAYMENT_CHOICES = (
+    ('CC', 'CREDIT_CARD'),
+    ('B', 'BLIK'),
+    ('C', 'CASH')
+)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    dishes = models.ManyToManyField(Dish)
+    payment = models.CharField(choices=PAYMENT_CHOICES, max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
