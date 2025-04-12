7350f682187f3280cffe68f86dc4893d998323d3
rodis00 <happyekkov@gmail.com>
2025-04-12 13:49:27 +0200

    settings update for images, register models in admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Dish)
+admin.site.register(models.Order)
+admin.site.register(models.Address)
+admin.site.register(models.Restaurant)
